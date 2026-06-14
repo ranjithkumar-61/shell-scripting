f968f12d0a09ef72e8de6686641b33126d9b0360
Ranjith Kumar E <ranjithkumar.e0601@gmail.com>
2026-06-15 01:06:39 +0530

    variables

---
+++ 02-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PERSON1=Suresh
+PERSON2=Ramesh
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing"
+echo "$PERSON1:: doing fine. What are you learning $PERSON2"
+echo "$PERSON2:: I am learning shell. What about you?"
+
+#echo "Suresh:: Hello Ramesh, How are you?"
+#echo "Ramesh:: Hi Suresh, I am fine thanks for asking. How are you doing"
+#echo "Suresh:: doing fine. What are you learning Ramesh"
+#echo "Ramesh:: I am learning shell. What about you?"
