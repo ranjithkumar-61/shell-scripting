e34b756e4bdb42ddda97411dc40cf64468017e47
Ranjith Kumar E <ranjithkumar.e0601@gmail.com>
2026-06-15 01:23:45 +0530

    arguments

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-PERSON1=Karan
-PERSON2=Rohit
+#Arguments pasing
+PERSON1=$1
+PERSON2=$2
+
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing"
 echo "$PERSON1:: doing fine. What are you learning $PERSON2"
 echo "$PERSON2:: I am learning shell. What about you?"
+
+
+#Variable declaration
+#PERSON1=Karan
+#PERSON2=Rohit
 
 #echo "Suresh:: Hello Ramesh, How are you?"
 #echo "Ramesh:: Hi Suresh, I am fine thanks for asking. How are you doing"
