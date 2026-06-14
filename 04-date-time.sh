c3724dfdc55fa56bf797b5c593201a5ce021b55a
Ranjith Kumar E <ranjithkumar.e0601@gmail.com>
2026-06-15 02:05:04 +0530

    start and end time

--- 04-date-time.sh
+++ 04-date-time.sh
@@ -4,3 +4,17 @@
 TIMESTAMP=$(date)
 
 echo "script executed at:: $TIMESTAMP"
+
+
+#start time and end time of the script
+
+START_TIME=$(date +%s)
+echo "Start time of the script :: $START_TIME"
+
+sleep 10
+
+END_TIME=$(date +%s)
+echo "End time of the script :: $END_TIME"
+
+TOTAL_TIME=$(($START_TIME-$END_TIME))
+echo "Total time taken to execute the script is $TOTAL_TIME"
