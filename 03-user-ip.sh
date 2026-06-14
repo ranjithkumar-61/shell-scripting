e63d18681444cbe02dab5248510f8a91c990e54c
Ranjith Kumar E <ranjithkumar.e0601@gmail.com>
2026-06-15 01:40:40 +0530

    userip

--- 03-user-ip.sh
+++ 03-user-ip.sh
@@ -9,4 +9,5 @@ echo "User name is $USER_NAME"
 echo "Please enter your password::"
 read -s PASSWORD     #It will encrypt the password
 
-echo "Password is $PASSWORD"     #It will encrypt the password
+
+echo "Password is $PASSWORD"     #It will decrypt the password
