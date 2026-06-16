dd03d7e9052b226fb9d4af63e3c2fa198045c719
Ranjith Kumar E <ranjithkumar.e0601@gmail.com>
2026-06-17 01:26:46 +0530

    script to install nginx

---
+++ 08-nginx-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Scipt to install the nginx
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]; then
+    echo "Please run the script with root user access"
+    exit 1
+fi
+
+echo "---Installing Nginx---"
+dnf insatll nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Insatlling Nginx...FAILURE"
+else
+    echo "Insatlling Nginx...SUCCESS"
+fi
