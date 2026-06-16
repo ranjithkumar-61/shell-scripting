ad0d92439d0d4f5441569cb38a5168fdb8da8e19
Ranjith Kumar E <ranjithkumar.e0601@gmail.com>
2026-06-16 22:52:03 +0530

    data-types

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 NUM1=100
-NUM2=300
+NUM2=300 ## if we declare here something as string by default it consdires as 0
 
 SUM=$(($NUM1+$NUM2))
 
 echo "Sum is :: $SUM"
+
+
+#Arrays
+
+Fruits=("Apple" "Bannana" "Pomo")
+echo "Fruits are :: ${Fruits[@]}"
+
+#Indexing
+
+echo "first fruit is :: ${Fruits[0]}"
+echo "second fruit is :: ${Fruits[1]}"
+echo "third fruit is :: ${Fruits[2]}"
