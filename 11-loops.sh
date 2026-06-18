62d7c739dbf086d6c00d718fc4e882ab99f3697c
Ranjith Kumar E <ranjithkumar.e0601@gmail.com>
2026-06-19 00:46:38 +0530

    loops

--- 11-loops.sh
+++ 11-loops.sh
@@ -4,8 +4,44 @@
 # echo 2
 # echo 3
 
-for i in {1...100}
-do
-    echo $i
+# for i in {1...100}
+# do
+#      echo $i
+# done
+
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run the script with root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+
+#If folder is not there it will create or else leave it
+mkdir -p $LOGS_FOLDER
+
+
+#$1, $2 argumnets
+VALIDATE(){
+if [ $1 -ne 0 ]; then
+    echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+    exit 1
+else
+    echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+fi
+}
+
+
+for package in $@ # sudo sh 11-loogs.sh mysql nodejs
+do 
+    dnf install $package -y &>>$LOGS_FILE
+    VALIDATE $? "Installing Nginx"
 done
+
+
+
 
