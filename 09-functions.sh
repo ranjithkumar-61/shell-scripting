90ff1e3aa645c4aacb4811d82c00a6d8e2612dfd
Ranjith Kumar E <ranjithkumar.e0601@gmail.com>
2026-06-18 21:37:02 +0530

    functions

---
+++ 09-functions.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#FUNCTIONS
+#functions are intended to complete specific task, it takes input and does the job
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run the script with root user access"
+    exit 1
+fi
+
+
+VALIDATE(){
+if [ $1 -ne 0 ]; then
+    echo "$2 ... FAILURE"
+    exit 1
+else
+    echo "$2 ... SUCCESS"
+fi
+}
+
+
+dnf install nginx -y
+VALIDATE $? "Installing Nginx"
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs"
+
+
+dnf install mysql -y
+VALIDATE $? "mysql"
+
