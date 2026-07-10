456e5842eaba374426015513a4a4060c0a6080ce
Ranjith Kumar E <ranjithkumar.e0601@gmail.com>
2026-07-10 13:28:24 +0530

    calling one script from another script

--- 17-script-1.sh
+++ 17-script-1.sh
@@ -7,5 +7,8 @@ echo "PID of script-1 is: $$"
 
 echo "Calling one script from another script"
 
+#By this pid will be different and variable calling is different"
+#sh 18-script-2.sh
 
-sh 18-script-2.sh
+#By this pid wiil be same and variable calling will be same";
+source ./18-scripot-2.sh
